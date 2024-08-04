9f90e26b16a7a089348f57013ba570cae9b0f71d
winbamstudios <willriddick@outlook.com>
2024-08-04 19:20:23 -0400

    upload code

---
+++ scripts/player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+const speed = 150
+var current_dir = "none"
+
+func _physics_process(delta: float) -> void:
+	player_movement(delta)
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walkright")
+		elif movement == 0:
+			anim.play("idleright")
+	elif dir == "left":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walkleft")
+		elif movement == 0:
+			anim.play("idleleft")
+	elif dir == "up":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walkbackward")
+		elif movement == 0:
+			anim.play("idleback")
+	elif dir == "down":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("walkforward")
+		elif movement == 0:
+			anim.play("idlefront")
+
+func player_movement(delta):
+	if Input.is_action_pressed("ui_right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
+		play_anim(1)
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		play_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
